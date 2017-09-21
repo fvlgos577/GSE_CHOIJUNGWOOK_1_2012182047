018c70ef36c85130e0fedba9b48883f68db459a9
fvlgos577 <fvlgos577>
2017-09-21 14:37:03 +0900

    Fisrt

---
+++ SimpleGame/SimpleGame/newObject.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#include "Dependencies\glew.h"
+
+#include "Renderer.h"
+
+
+class newObject : public Renderer {
+public:
+
+};
